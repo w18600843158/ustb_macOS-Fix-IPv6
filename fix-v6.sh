c096b404438a3ed2a6af37cf7eefe456c413fa3c
Jasper0819X <wanjixiang819@hotmail.com>
2018-04-18 13:31:47 +0800

    Rewrite script using networksetup

--- fix-v6.sh
+++ fix-v6.sh
@@ -1,23 +1,53 @@
 #!/usr/bin/env sh
 
-# get active interface
-INTERFACE=$(route get google.com | awk '/interface:/{print $2}')
-# reset first
-sudo ipconfig set $INTERFACE automatic-v6
-sleep 0.5
-# get ip/prefix length/default router
-IP=$(ifconfig | awk '/inet6 .*autoconf/{print $2}')
-PREFIX_LEN=$(ifconfig | awk '/inet6 .*autoconf/{print $4}')
-echo "IP:        $IP/$PREFIX_LEN"
-echo "Interface: $INTERFACE"
+services_raw=$(networksetup -listnetworkserviceorder | tail -n +2)
+# remove \n in services_raw
+services=$(echo $services_raw | sed "s/(\([0-9]\))/\\\n/g")
 
-ROUTER=$(traceroute6 -m 1 bt.byr.cn 2>/dev/null | awk '{print $2}')
-echo "Router:    $ROUTER"
-# manual set IP 
-sudo ifconfig $INTERFACE inet6 $IP/$PREFIX_LEN
-# restart interface
-sudo ifconfig $INTERFACE down
-sleep 0.5
-sudo ifconfig $INTERFACE up
-# manual add default route
-sudo route add -inet6 -prefixlen 0 default $ROUTER
+while read line; do
+    sname=$(echo $line | sed "s/(\(.*\))//g" | sed "s/ $//g")
+    sdev=$(echo $line | awk -F  "(,)|(: )|[)]" '{print $4}')
+    if [ -n "$sdev" ]; then
+        ifout="$(ifconfig $sdev 2>/dev/null)"
+        echo "$ifout" | grep 'status: active' > /dev/null 2>&1
+        rc="$?"
+        if [ "$rc" -eq 0 ]; then
+            current_service="$sname"
+            current_device="$sdev"
+        fi
+    fi
+done <<< "$(echo "$services")"
+
+if [ -n "$current_service" ]; then
+    echo "Current Service: '$current_service'"
+    echo "Current Device:  $current_device"
+else
+    >&2 echo "Could not find current service"
+    exit 1
+fi
+
+method=$(networksetup -getinfo "$current_service" | grep "IPv6: " | sed "s/^IPv6: //")
+echo "Current IPv6 Config: $method"
+
+if [ "$method" != "Automatic" ]; then 
+    echo "Reset to automatic..."
+    networksetup -setv6automatic "$current_service"
+    echo  "wait for address..."
+    sleep 1;
+fi
+
+IP=$(ifconfig $current_device | awk '/inet6 .*autoconf/{print $2}')
+PREFIX_LEN=$(ifconfig $current_device | awk '/inet6 .*autoconf/{print $4}')
+router=$(traceroute6 -m 1 bt.byr.cn 2>/dev/null | awk '{print $2}')
+
+if [ -n "$IP" ]; then
+    echo "IP:        $IP/$PREFIX_LEN"
+    echo "Router     $router"
+
+    echo "Set static IP..."
+    networksetup -setv6manual "$current_service" $IP $PREFIX_LEN $router
+    echo "Done."
+else
+    >&2 echo "Could not find autoconf IP."
+    exit 1
+fi
--- reset-v6.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-INTERFACE=$(route get google.com | awk '/interface:/{print $2}'
-sudo ipconfig set $INTERFACE automatic-v6
